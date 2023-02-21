6153495c30895a6ac658b2a97b08106a5d1f20af
GGO-web <iicandyoii@gmail.com>
2023-02-21 11:16:03 +0200

    Added header hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,48 +1,76 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 import {Link} from 'react-router-dom';
 
 export const Header = () => {
+  const iconMenu = useRef<HTMLButtonElement>(null);
+
+  function menuToggle() {
+    if (iconMenu.current?.getAttribute('aria-expanded') === 'true') {
+      document.documentElement.classList.remove('menu-open');
+      iconMenu.current.setAttribute('aria-expanded', 'false');
+    } else if (iconMenu.current?.getAttribute('aria-expanded') === 'false') {
+      document.documentElement.classList.add('menu-open');
+      iconMenu.current.setAttribute('aria-expanded', 'true');
+    }
+  }
+
   return (
     <header className="header">
       <div className="header__container container">
-        <nav className="menu">
-          <ul className="menu__list list-reset">
-            <li className="menu__list-item">
-              <Link to="/" className="menu__link">
-                Фільми
-              </Link>
-            </li>
-            <li className="menu__list-item">
-              <Link to="/" className="menu__link">
-                Серіали
-              </Link>
-            </li>
-            <li className="menu__list-item">
-              <Link to="/" className="menu__link">
-                Мульти
-              </Link>
-            </li>
-            <li className="menu__list-item">
-              <Link to="/" className="menu__link">
-                Форум
-              </Link>
-            </li>
-            <li className="menu__list-item">
-              <Link to="/" className="menu__link">
-                Топ 100
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <div className="menu">
+          <button
+            ref={iconMenu}
+            className="menu__icon icon-menu btn-reset"
+            aria-label="Відкрити головне меню"
+            aria-expanded="false"
+            aria-controls="menu"
+            onClick={menuToggle}
+          >
+            <span />
+          </button>
 
-        <div className="button-group" style={{display: 'flex', gap: '30px'}}>
-          <Link to="/login" className="button-style">
-            Вхід
-          </Link>
-          <Link to="/registration" className="button-style button-style_stroke">
-            Реєстрація
-          </Link>
+          <nav className="menu__body">
+            <ul className="menu__list list-reset">
+              <li className="menu__item">
+                <Link to="/" className="menu__link">
+                  Фільми
+                </Link>
+              </li>
+              <li className="menu__item">
+                <Link to="/" className="menu__link">
+                  Серіали
+                </Link>
+              </li>
+              <li className="menu__item">
+                <Link to="/" className="menu__link">
+                  Мульти
+                </Link>
+              </li>
+              <li className="menu__item">
+                <Link to="/" className="menu__link">
+                  Форум
+                </Link>
+              </li>
+              <li className="menu__item">
+                <Link to="/" className="menu__link">
+                  Топ 100
+                </Link>
+              </li>
+            </ul>
+
+            <div className="button-group">
+              <Link to="/login" className="button-style">
+                Вхід
+              </Link>
+              <Link
+                to="/registration"
+                className="button-style button-style_stroke"
+              >
+                Реєстрація
+              </Link>
+            </div>
+          </nav>
         </div>
       </div>
     </header>
