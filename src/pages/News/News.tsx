cd9deb1e1ede03559fe0d594936bb8b710503e63
Kos <turchakkonstantin@gmail.com>
2023-08-12 16:24:04 +0300

    add default News.tsx

---
+++ src/pages/News/News.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { Header } from '@components/Header/Header';
+import { Sidenav } from '@components/Navigation/Sidenav';
+import { Hero } from '@components/Hero/Hero';
+import { Heading } from '@components/Heading/Heading';
