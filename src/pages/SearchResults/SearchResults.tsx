a8a55be8929e5899acf6cbdb58ec29e41fa8ffaa
Alexey Sevastynov <alexseva94@gmail.com>
2023-12-09 17:09:42 +0200

    added block to page SearchResults: seo text, NextSeasons, Recommendations

--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -10,6 +10,10 @@ import { Search } from '@components/Searchbar/Search';
 import { ExtraFilters } from '@components/Searchbar/ExtraFilters';
 import { useQuery } from '@tanstack/react-query';
 import { TitleService } from '@services/Title/Title.service';
+import { Paragraph } from '@components/Paragraph/Paragraph';
+import { NextSeasons } from '@pages/Title/components/NextSeasons/NextSeasons';
+import { Recommendations } from '@pages/Title/components/Recommendations/Recommendations';
+import { NEXT_ANIME, RECOMMENDATIONS_ANIME } from '@/constants';
 
 export const SearchResults = () => {
   const { data: newTitles } = useQuery({
@@ -46,8 +50,28 @@ export const SearchResults = () => {
               </div>
             )}
 
-            <Heading>Результати пошуку... (12)</Heading>
+            <Heading className="mb-[67px]">Результати пошуку... (12)</Heading>
+          </div>
+          <div className="container">
             <ListTitles items={newTitles} />
+
+            <div className="search-results__footer ">
+              <div className="search-results__footer-col-1">
+                <strong>
+                  <Heading className="text-xl mb-5">СЕО-ТЕКСТ</Heading>
+                </strong>
+                <Paragraph className="mb-[63px]">
+                  {
+                    " Мультфільм, який є продовженням однойменного серіалу, розповість про пригоди Джона Костянтина - приватного детектива, який володіє знаннями з окультизму і демонології та розслідує загадкові події, пов'язані з проявами потойбічних сил. Цього разу йому належить розібратися, що ж сталося з восьмирічною донькою старого друга по імені Чез, яка несподівано впала в кому. Незважаючи на ретельне обстеження,лікарі так і не змогли встановити причину, тому Чез абсолютно впевнений, що його дочка перебуває під впливом темної магії"
+                  }
+                </Paragraph>
+              </div>
+
+              <div className="search-results__footer-col-2">
+                <NextSeasons anime={NEXT_ANIME} />
+                <Recommendations anime={RECOMMENDATIONS_ANIME} />
+              </div>
+            </div>
           </div>
         </main>
       </div>
