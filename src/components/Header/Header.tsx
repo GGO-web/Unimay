98396a44a92463bee08ba2e39c0b7e168e1168c9
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-12-23 12:46:08 +0200

    fixed Header, hide elements (#64)
    * Hided buttons sign in/out from Header
    
    * hid news from main navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { navigationItems } from '../../constants';
 
 export const Header = () => {
   const iconMenu = useRef<HTMLButtonElement>(null);
+  const [isVisibleBtnsAuth, setIsVisibleBtnsAuth] = useState<boolean>(false);
 
   function menuToggle() {
     if (iconMenu.current?.getAttribute('aria-expanded') === 'true') {
@@ -34,28 +35,32 @@ export const Header = () => {
           <nav className="menu__body">
             <ul className="menu__list list-reset">
               {navigationItems.map((navItem) => {
-                return (
-                  <li key={navItem.id} className="menu__item">
-                    <Link to={navItem.href} className="menu__link">
-                      {navItem.text}
-                    </Link>
-                  </li>
-                );
+                if (navItem.text !== 'Новини') {
+                  return (
+                    <li key={navItem.id} className="menu__item">
+                      <Link to={navItem.href} className="menu__link">
+                        {navItem.text}
+                      </Link>
+                    </li>
+                  );
+                }
               })}
             </ul>
 
-            <div className="button-group">
-              <Link to="/login" className="button-style">
-                Вхід
-              </Link>
+            {isVisibleBtnsAuth && (
+              <div className="button-group">
+                <Link to="/login" className="button-style">
+                  Вхід
+                </Link>
 
-              <Link
-                to="/registration"
-                className="button-style button-style_stroke"
-              >
-                Реєстрація
-              </Link>
-            </div>
+                <Link
+                  to="/registration"
+                  className="button-style button-style_stroke"
+                >
+                  Реєстрація
+                </Link>
+              </div>
+            )}
           </nav>
         </div>
       </div>
